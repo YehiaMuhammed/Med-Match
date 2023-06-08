2cbdfbb0295d7b96d9ec3d18411a6b4165bcc1ef
YehiaMuhammed <yehiamohamed030@gmail.com>
2023-06-09 00:38:57 +0300

    added search for edited drug in combobox

--- Med Match/Forms/EditDrugs.Designer.cs
+++ Med Match/Forms/EditDrugs.Designer.cs
@@ -28,20 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.components = new System.ComponentModel.Container();
+            this.cbDrugs = new System.Windows.Forms.ComboBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cbDrugs
             // 
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(194, 176);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(228, 33);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.Text = "Drug";
+            this.cbDrugs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbDrugs.FormattingEnabled = true;
+            this.cbDrugs.Location = new System.Drawing.Point(194, 176);
+            this.cbDrugs.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.cbDrugs.Name = "cbDrugs";
+            this.cbDrugs.Size = new System.Drawing.Size(228, 33);
+            this.cbDrugs.TabIndex = 0;
+            this.cbDrugs.Text = "Drug";
+            this.cbDrugs.TextChanged += new System.EventHandler(this.cbDrugs_TextChanged);
             // 
             // panel2
             // 
@@ -52,12 +55,17 @@
             this.panel2.Size = new System.Drawing.Size(235, 1);
             this.panel2.TabIndex = 7;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // EditDrugs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbDrugs);
             this.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Name = "EditDrugs";
@@ -68,7 +76,8 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbDrugs;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Med Match/Forms/EditDrugs.cs
+++ Med Match/Forms/EditDrugs.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Med_Match.Forms
 {
     public partial class EditDrugs : UserControl
     {
+        IMongoCollection<BsonDocument> collection;
         public EditDrugs()
         {
             InitializeComponent();
+            var connectionString = "mongodb+srv://admin:cdoGLvJ6bxxneHQn@cluster0.a0hbnon.mongodb.net";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Project");
+            collection = database.GetCollection<BsonDocument>("drugs");
+        }
+
+        private void cbDrugs_TextChanged(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Start();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+
+            // Wait for the timer to elapse
+            await System.Threading.Tasks.Task.Delay(timer1.Interval);
+
+            // Retrieve the entered drug name
+            var drugName = cbDrugs.Text.Trim();
+
+            if (!string.IsNullOrEmpty(drugName))
+            {
+
+                // Perform a search in the drugs collection
+                var filter = Builders<BsonDocument>.Filter.Regex("name", new BsonRegularExpression($"^{drugName}", "i"));
+                var similarDrugs = await collection.Find(filter).ToListAsync();
+
+                // Clear existing items and add similar drug names to the ComboBox
+                cbDrugs.Items.Clear();
+                foreach (var drug in similarDrugs)
+                {
+                    var similarDrugName = drug["name"].AsString;
+                    cbDrugs.Items.Add(similarDrugName);
+                }
+            }
         }
     }
 }
